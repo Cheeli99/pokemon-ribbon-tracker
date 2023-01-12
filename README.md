c96fe0f15c3f1d0d8d804f740e61320808ffa6db
cheeli99 <ejmccarthy474@protonmail.com>
2023-01-12 13:39:50 -0600

    Marks section added

--- README.md
+++ README.md
@@ -6,4 +6,10 @@
   and save it for later use (check the privacy policy for more information).
 
 ## Changelog
+- v2.0 - Marks can now be tracked in Pokemon SwSh and Pokemon SV (Jan 12, 2023)
+
+- v1.2 - Pokemon Scarlet and Violet added (December 29, 2022)
+
+- v1.1 - Pokemon Brilliant Diamond and Shining Pearl added (Jan 10, 2022)
+
 - v1.0 - First release
--- assets/js/main.js
+++ assets/js/main.js
@@ -1,3 +1,5 @@
+
+/*
 // Add a "checked" symbol when clicking on a list item
 var list = document.querySelector('tr');
 list.addEventListener('click', function(ev) {
@@ -5,20 +7,84 @@ list.addEventListener('click', function(ev) {
     ev.target.classList.toggle('checked');
   }
 }, false);
+*/
+
+/** 
+// Creates a json file with each checkbox, storing if the checkbox is checked or not
+$('.custom-control-input').on('click', function() {
+  var fav, favs = [];
+  $('.custom-control-input').each(function() { // run through each of the checkboxes
+    fav = {id: $(this).attr('id'), value: $(this).prop('checked')};
+    favs.push(fav);
+  });
+  localStorage.setItem("ribbonList", JSON.stringify(favs));
+
+  console.log(favs);
+});
+
+// Parses through the created json file and reasigns values to the checkboxes
+$(document).ready(function() {
+  var list = JSON.parse(localStorage.getItem('ribbonList'));
+  if (!list.length) {return};
+  console.debug(list);
+
+  for (var i = 0; i < list.length; i++) {
+    console.debug(list[i].value == 'on');
+    $('#' + list[i].id ).prop('checked', list[i].value);
+  }
+});
+
+function clearAll() {
+  location.reload();
+  localStorage.clear();
+}
+
+function addJSON() {
+  
+}
+*/
+
+/*
+function save() {
+  ribbonList = JSON.parse(localStorage.getItem('ribbonList'));
+}
+
+function save1() {
+  var checkbox = document.getElementById('tableDefaultCheck1');
+  localStorage.setItem('tableDefaultCheck1', checkbox.checked);
+}
+
+function load1() {
+  var checked = JSON.parse(localStorage.getItem('tableDefaultCheck1'));
+  document.getElementById("tableDefaultCheck1").checked = checked;
+}
+
+function load() {
+  var checked = JSON.parse(localStorage.getItem('1'));
+  document.getElementById("1").checked = checked;
+}
 
 // Select all check boxes : Setting the checked property to true in checkAll() function
 function checkAll(){
-  var items = document.getElementsByName('ribbon');
-    for (var i = 0; i < items.length; i++) {
-        if (items[i].type == 'checkbox')
-            items[i].checked = true;
+  ribbonList = document.getElementsByName('ribbon');
+    for (var i = 0; i < ribbonList.length; i++) {
+        if (ribbonList[i].type == 'checkbox')
+        ribbonList[i].checked = true;
     }
+    completedList = ribbonList;
+    console.log(completedList);
+    return completedList;
 }
+
 // Clear all check boxes : Setting the checked property to false in uncheckAll() function
 function uncheckAll(){
-  var items = document.getElementsByName('ribbon');
-    for (var i = 0; i < items.length; i++) {
-        if (items[i].type == 'checkbox')
-            items[i].checked = false;
+  ribbonList = document.getElementsByName('ribbon');
+    for (var i = 0; i < ribbonList.length; i++) {
+        if (ribbonList[i].type == 'checkbox')
+        ribbonList[i].checked = false;
     }
+    return ribbonList;
 }
+
+load();
+*/
